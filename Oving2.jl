f49c2d1eb37f2c3deeda0871787f804109c42c69
“martinaalien” <martin.aalien@outlook.com>
2019-09-12 20:13:06 +0200

    Finished Oving2

--- Oving2.jl
+++ Oving2.jl
@@ -1,6 +1,14 @@
+testarray = [10, 6, 7, 3, 7, 77]
+
 mutable struct Node
     next::Union{Node, Nothing} # next kan peke på et Node-objekt eller ha verdien nothing.
     value::Int
+end
+
+mutable struct NodeDoublyLinked
+    prev::Union{NodeDoublyLinked, Nothing} # Er enten forrige node eller nothing
+    next::Union{NodeDoublyLinked, Nothing} # Er enten neste node eller nothing
+    value::Int # Verdien til noden
 end
 
 function createlinkedlist(length)
@@ -16,23 +24,68 @@ function createlinkedlist(length)
     return node
 end
 
-function findindexinlist(linkedlist, index)
-    i = 0
-    VALUEVAR = 0
-    NEXTVAR = Union{Node, Nothing}
-    if (false)
-        return -1
-    else
-        while(i < index)
-            NEXTVAR = linkedlist.next
-            i += 1
+function createLinkedListDoublyLinked(length)
+    # Create the list from the last element in the chain
+    # This is so the returned element will be the first in the chain
+    current = nothing
+    beforeCurrent = nothing
+
+    for i in 1:length
+        # only fill out the next field because prev will be filled out later
+        current = NodeDoublyLinked(nothing, beforeCurrent, rand(-100:100))
+        # link up the node before this node to this node
+        if beforeCurrent != nothing
+            beforeCurrent.prev = current
         end
-        return linkedlist.value
-    end 
+        beforeCurrent = current
+    end
+    return current
 end
 
-node = createlinkedlist(10)
+function findindexinlist(linkedlist, index)
+    i = 1
+    while(i < index && linkedlist.next != nothing)
+        linkedlist = linkedlist.next
+        i += 1
+    end
+    if (i < index)
+        return -1
+    else
+        return linkedlist.value
+    end
+end 
 
-println(findindexinlist(node, 5))
-println(node)
+function reverseandlimit(array, maxnumber)
+    array = reverse(array)
+    for i in 1:length(array)
+        if (array[i] > maxnumber)
+            array[i] = maxnumber
+        end
+    end
+    return array
+end
+
+function maxofdoublelinkedlist(linkedlist)
+x = linkedlist
+max = linkedlist.value
+while(x != nothing)
+    if (max < x.value)
+        max = x.value
+    end
+    x = x.next
+end
+while(linkedlist != nothing)
+    if(max < linkedlist.value)
+        max = linkedlist.value
+    end
+    linkedlist = linkedlist.prev
+end
+return max
+end
+
+println(findindexinlist(createlinkedlist(10), 11))
+
+println(reverseandlimit(testarray, 10))
+
+println(maxofdoublelinkedlist(createLinkedListDoublyLinked(10)))
 
