333924416488a82490b5233b8021eb1437398d7a
“martinaalien” <martin.aalien@outlook.com>
2019-10-06 18:10:52 +0200

    Question 4 Oving5 tmp commit

--- Oving5.jl
+++ Oving5.jl
@@ -53,7 +53,7 @@ function searchtree(root, dna)
     i = 1
     dnaend = length(dna)
     currentnode = root
-    while (i <= dnaend)
+    for i in 1:dnaend
         if (!haskey(currentnode.children, dna[i]))
             return 0
         end
@@ -61,7 +61,6 @@ function searchtree(root, dna)
         if (i == dnaend)
             return currentnode.count
         end
-        i +=1
     end
     return 0
 end
@@ -182,4 +181,93 @@ end
 println("\nFungerte alt? Prøv å kjør koden i inginious!")
 println("Husk at disse testene ikke alltid sjekker alle edge-cases")
 println("---------------------------------------------------------\n\n")
+##########################################################################
+
+##########################################################################
+#
+#   Søk med ødelagt DNA 
+#
+##########################################################################
+### Denne må med når en tester lokalt ###
+# (try-catch er bare for å unngå feilmelding om redefinition of constant struct)
+try
+	mutable struct Node
+    	children::Dict{Char,Node}
+    	count::Int
+	end
+catch
+	println("Node() allerede definert")
+end
+
+
+
+
+### Du skal implementere denne funksjonen ###
+function brokendnasearch(root, dna, i=1, sum=0)
+    if (i <= length(dna))
+        currentnode = root
+        if (dna[i] == '?')
+            # possiblerouts = filter(x -> x == "ACTG", currentnode.children)
+            # println(currentnode.children)
+            sum += 5
+            i += 1
+            brokendnasearch(root, dna, i, sum)
+        else
+            currentnode = currentnode.children[dna[i]]
+            i += 1
+            brokendnasearch(root, dna, i, sum)
+        end
+    else
+        return sum
+    end
+end
+
+println(brokendnasearch(root2, s4))
+
+
+### Konstruert testdata, la stå ###
+root1 = Node(Dict('A' => Node(Dict{Char,Node}(), 1)), 0)
+root2 = Node(Dict('A' => Node(Dict('G' => Node(Dict('T' => Node(Dict('A' => 
+        Node(Dict{Char,Node}(), 1),'T' => Node(Dict{Char,Node}(), 1),'C' => 
+        Node(Dict{Char,Node}(), 1)), 4)), 5)), 5)), 0)
+root3 = Node(Dict('C' => Node(Dict('C' => Node(Dict('C' => Node(Dict('C' => 
+        Node(Dict('C' => Node(Dict('C' => Node(Dict{Char,Node}(), 1)), 2)), 3)), 4)), 5)), 6)), 0)
+
+s1 = "A"
+s2 = "T"
+s3 = "?"
+s4 = "????"
+s5 = "C?C"
+s6 = "???"
+s7 = "?????"
+
+
+#= 
+
+### Tester ###
+# Disse testene blir kjør når du kjører filen
+# Du trenger ikke å endre noe her, men du kan eksperimentere!
+
+printstyled("\n\n\n---------------\nKjører tester!!\n---------------\n"; color = :magenta)
+
+            sum += 5
+using Test
+@testset "Tester" begin
+	@test brokendnasearch(root1, s1) == 1
+	@test brokendnasearch(root1, s2) == 0
+	@test brokendnasearch(root1, s3) == 1
+	@test brokendnasearch(root1, s4) == 0
+	@test brokendnasearch(root2, s1) == 5
+	@test brokendnasearch(root2, s3) == 5
+	@test brokendnasearch(root2, s4) == 5
+	@test brokendnasearch(root2, s5) == 0
+	@test brokendnasearch(root2, s6) == 4
+	@test brokendnasearch(root3, s5) == 4
+	@test brokendnasearch(root3, s6) == 4
+	@test brokendnasearch(root3, s7) == 2
+end
+
+println("\nFungerte alt? Prøv å kjør koden i inginious!")
+println("Husk at disse testene ikke alltid sjekker alle edge-cases")
+println("---------------------------------------------------------\n\n") =#
 ##########################################################################
