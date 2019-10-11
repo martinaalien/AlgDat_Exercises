68504890c6caf6031e642da6ffe50e96c4f25d0f
“martinaalien” <martin.aalien@outlook.com>
2019-10-12 01:07:16 +0200

    Question 2 Oving6 finished. Question 2 started

--- Oving6.jl
+++ Oving6.jl
@@ -41,3 +41,78 @@ println("\nFungerte alt? Prøv å kjør koden i inginious!")
 println("Husk at disse testene ikke alltid sjekker alle edge-cases")
 println("---------------------------------------------------------\n\n")
 ##########################################################################
+
+##########################################################################
+#
+#    Finne den lengste økende delfølgen 
+#
+##########################################################################
+function lis(s, mls)
+    ml = maximum(mls)
+    lis = zeros(Int, ml)
+    for i = length(mls):-1:1
+        if (mls[i] == ml)
+            lis[ml] = s[i]
+            ml -=1
+        end
+    end
+    return lis
+end
+
+### Tester ###
+# Disse testene blir kjør når du kjører filen
+# Du trenger ikke å endre noe her, men du kan eksperimentere!
+
+printstyled("\n\n\n---------------\nKjører tester!!\n---------------\n"; color = :magenta)
+
+using Test
+@testset "Tester" begin
+	@test lis([5,3,3,6,7],[1, 1, 1, 2, 3]) == [3,6,7]
+	@test lis([2,2,2,2],[1, 1, 1, 1]) == [2]
+	@test lis([100,50,25,10],[1, 1, 1, 1]) == [10]
+    @test lis([0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15],
+    [1, 2, 2, 3, 2, 3, 3, 4, 2, 4, 3, 5, 3, 5, 4, 6]) == [0,2,6,9,11,15]
+end
+
+println("\nFungerte alt? Prøv å kjør koden i inginious!")
+println("Husk at disse testene ikke alltid sjekker alle edge-cases")
+println("---------------------------------------------------------\n\n")
+##########################################################################
+
+##########################################################################
+#
+#    Finn vektene 
+#
+##########################################################################
+function cumulative(weights)
+    rows, cols = size(matrix)
+    min = -Inf
+    for i in 1:rows-1
+        min = minimum(weights[1, 1], weights[2, 1])
+        weights[1, 2] = min
+        for j in 2:cols-1
+           
+        end
+
+    end
+end
+
+
+### Tester ###
+# Disse testene blir kjør når du kjører filen
+# Du trenger ikke å endre noe her, men du kan eksperimentere!
+
+printstyled("\n\n\n---------------\nKjører tester!!\n---------------\n"; color = :magenta)
+
+using Test
+@testset "Tester" begin
+	@test cumulative([1 1 ; 1 1]) == [1 1 ;2 2]
+    #Dette er samme eksempel som det vist i oppgaveteskten
+	@test cumulative([3  6  8 6 3; 7  6  5 7 3; 4  10 4 1 6; 10 4  3 1 2;6  1  7 3 9])== [3  6  8  6  3;10 9  11 10 6;13 19 13 7  12;23 17 10 8  9;23 11 15 11 17]
+end
+
+
+println("\nFungerte alt? Prøv å kjør koden i inginious!")
+println("Husk at disse testene ikke alltid sjekker alle edge-cases")
+println("---------------------------------------------------------\n\n")
+##########################################################################
