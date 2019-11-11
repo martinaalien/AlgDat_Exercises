1ccc87d6d3cb9d4fb7c674b6f52d7d59b75dd285
martinaalien <martin.aalien@outlook.com>
2019-11-11 20:09:09 +0100

    Oving10 question 2 done

--- Oving10.jl
+++ Oving10.jl
@@ -39,3 +39,25 @@ function general_dijkstra!(G, w, s, reverse=false)
     end
 end
 ##########################################################################
+
+##########################################################################
+#
+#   Widest path 
+#
+##########################################################################
+function initialize!(G, s)
+    for v in G.V
+       v.d = -typemax(Float64)
+       v.p = nothing
+    end
+    s.d = typemax(Float64)
+end
+
+
+function update!(u, v, w)
+    if (v.d < min(u.d, w[(u, v)]))
+        v.d = min(u.d, w[(u, v)])
+        v.p = u 
+    end
+end
+##########################################################################
